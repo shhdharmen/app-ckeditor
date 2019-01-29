5837e74682bea129a97065cd4dcc6979f51fd4df
dharmen.s <shhdharmen@gmail.com>
2019-01-29 14:12:14 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-ckeditor';
+  public Editor = ClassicEditor;
 }
